aa19a4e4759b7a61a86ec7a73bab2645875b4883
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2015-05-18 00:16:51 -0400

    minor code cleanup in post_processor_plugin

--- vigir_footstep_planning_lib/src/plugins/post_process_plugin.cpp
+++ vigir_footstep_planning_lib/src/plugins/post_process_plugin.cpp
@@ -3,12 +3,12 @@
 namespace vigir_footstep_planning
 {
 PostProcessPlugin::PostProcessPlugin(const std::string& name, const ParameterSet& params)
-  : Plugin("post_processor", "post_process_plugin", params)
+  : Plugin(name, "post_process_plugin", params)
 {
 }
 
 PostProcessPlugin::PostProcessPlugin(const std::string& name)
-  : Plugin("post_processor", "post_process_plugin")
+  : Plugin(name, "post_process_plugin")
 {
 }
 
@@ -17,15 +17,15 @@ bool PostProcessPlugin::isUnique() const
   return false;
 }
 
-void PostProcessPlugin::postProcessStepForward(const State& /*left*/, const State& /*right*/, State& /*swing*/) const
+void PostProcessPlugin::postProcessStepForward(const State& /*left_foot*/, const State& /*right_foot*/, State& /*swing_foot*/) const
 {
 }
 
-void PostProcessPlugin::postProcessStepBackward(const State& /*left*/, const State& /*right*/, State& /*swing*/) const
+void PostProcessPlugin::postProcessStepBackward(const State& /*left_foot*/, const State& /*right_foot*/, State& /*swing_foot*/) const
 {
 }
 
-void PostProcessPlugin::postProcessStep(const msgs::Step& /*left*/, const msgs::Step& /*right*/, msgs::Step& /*swing*/, msgs::StepPlan& /*step_plan*/) const
+void PostProcessPlugin::postProcessStep(const msgs::Step& /*left_foot*/, const msgs::Step& /*right_foot*/, msgs::Step& /*swing_foot*/, msgs::StepPlan& /*step_plan*/) const
 {
 }
 
@@ -37,25 +37,25 @@ void PostProcessPlugin::postProcess(msgs::StepPlan step_plan) const
   std::vector<msgs::Step>::iterator itr = step_plan.steps.begin();
 
   // determine start configuration
-  msgs::Step left, right = *itr;
+  msgs::Step left_foot, right_foot = *itr;
 
-  if (left.foot.foot_index != msgs::Foot::LEFT)
-    left.foot = step_plan.start.left;
+  if (left_foot.foot.foot_index != msgs::Foot::LEFT)
+    left_foot.foot = step_plan.start.left;
 
-  if (right.foot.foot_index != msgs::Foot::RIGHT)
-    right.foot = step_plan.start.right;
+  if (right_foot.foot.foot_index != msgs::Foot::RIGHT)
+    right_foot.foot = step_plan.start.right;
 
   // iterate step plan
   for (; itr != step_plan.steps.end(); itr++)
   {
-    msgs::Step& swing = *itr;
+    msgs::Step& swing_foot = *itr;
 
-    postProcessStep(left, right, swing, step_plan);
+    postProcessStep(left_foot, right_foot, swing_foot, step_plan);
 
-    if (swing.foot.foot_index == msgs::Foot::LEFT)
-      left = swing;
+    if (swing_foot.foot.foot_index == msgs::Foot::LEFT)
+      left_foot = swing_foot;
     else
-      right = swing;
+      right_foot = swing_foot;
   }
 }
 }
